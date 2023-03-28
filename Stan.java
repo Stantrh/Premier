cd6779f9b851cc09dd30f6ab45e78db00ac9aeca
Nathan PIERROT <nathan.pierrot88@gmail.com>
2023-03-28 15:23:36 +0200

    définition des methodes et constructeur

--- Stan.java
+++ Stan.java
@@ -1,6 +1,18 @@
 public class Stan{
 
-    private String train;
+    private String nomTrain;
     private final String BUS = "Stan est nul à minecraft";
+
+    public Stan(String nom){
+        this.nomTrain = nom;
+    }
+
+    public void changerNom(String nouveauNom){
+        this.nomTrain = nouveauNom;
+    }
+
+    public String getNom(){
+        return this.nomTrain;
+    }
 
 }
